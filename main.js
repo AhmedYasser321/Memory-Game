b25b1287fed71a54f39667771a719c72f307e924
Ahmed Yasser <ahmedyassermansour111@gmail.com>
2024-12-31 23:39:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,108 +1,46 @@
-
-let control = document.querySelector(".control span")
-
-let gameBlock = document.querySelector(".game-block")
+let control = document.querySelector('.control'); 
+let start = document.querySelector('.control span'); 
+let stopWatch = document.querySelector('.stopwatch span'); 
+let tries = document.querySelector('.tries span'); 
+let gameBlock = document.querySelector('.game-block'); 
+let memoryContainer = document.querySelector('.memory'); 
+let memory = document.querySelectorAll('.memory .game-block');
 
 control.onclick = function(){
 let yourName = window.prompt('please, Write Name')
 if(yourName === null || yourName === ""){
-    document.querySelector(".info .name span").innerHTML = "Unknown"
+    document.querySelector(".info .name span").innerHTML = "Sir"
 }else{
     document.querySelector(".info .name span").innerHTML = yourName
 }
 document.querySelector(".control").remove()
 }
 
-let memory = document.querySelector(".memory")
+let blocks = [...memory];
+let range = [...Array(memory.length).keys()];
+let check;
 
-let blocks = Array.from(memory.children)
-let blocksKeys = [...blocks.keys()]
+shuffle(range);
 
-shuffle(blocksKeys);
-
-blocks.forEach((block,index)=>{
-    block.style.order = blocksKeys[index]
-    block.addEventListener("click", ()=>{
-        flip(block) 
-    })
-})
-
-let duration = 1000;
-function flip(selected){
-    selected.classList.add("is-flipped")
-   let allflip = blocks.filter((block)=>block.classList.contains("is-flipped"))
-   if(allflip.length === 2){
-    memory.classList.add("no-click")
-    setTimeout(()=>{
-        memory.classList.remove("no-click")
-    },duration)
-    checkblocks(allflip[0],allflip[1]);
-   }
-  
-}
-
-let tries = document.querySelector(".tries span");
-
-function checkblocks(first,second){
-if(first.dataset.technology === second.dataset.technology){
-   first.classList.remove("is-flipped");
-   second.classList.remove("is-flipped");
-   first.classList.add("identical");
-   second.classList.add("identical");
-   checkAllIdentical();
-   document.querySelector(".success").play()
-}else{
-   tries.innerHTML = parseInt(tries.innerHTML) + 1;
-  
-   setTimeout(()=>{
-    first.classList.remove("is-flipped");
-   second.classList.remove("is-flipped");
-},duration)
-document.querySelector(".fail").play()
-}
-}
-
-function shuffle(arr){
-let current = arr.length
-while(current > 0){
-    let random = Math.floor(Math.random() * current);
-    current--
-    let value = arr[current];
-    arr[current] = arr[random];
-    arr[random] = value;
-}
-return arr
-}
-
-
+let interval = true;
 
 let message = document.createElement("div");
 
 document.body.appendChild(message);
 
-function checkAllIdentical() {
-    const allHaveClass = blocks.every(element => element.classList.contains('identical'));
-    if (allHaveClass) {
-        message.innerHTML = "You Win!";
-        message.className = "popup";
-    }
-}
-
-
-let stop = document.querySelector(".info .stopwatch span");
-let interval = true;
-
-blocks.forEach((block)=>{
+blocks.forEach((block, index)=>{
+    block.style.order = range[index];
     block.addEventListener("click",()=>{
-       if(interval === true){
+        flipblock(block);
+       if(interval){
        let counter = setInterval(()=>{
-            stop.innerHTML-=1
-            if(stop.innerHTML === "0" || message.innerHTML === "You Win!" || tries.innerHTML === "5"){
+            stopWatch.innerHTML-=1
+            if(stopWatch.innerHTML === "0" || message.innerHTML === "You Win!" || tries.innerHTML === "5"){
                 clearInterval(counter)
                if(message.innerHTML !== "You Win!"){
+                   message.className = "popup";
                 message.innerHTML = "Game over!";
-                message.className = "popup";
-                memory.classList.add("no-click")
+                memoryContainer.classList.add("no-click")
                 document.querySelector(".game-over").play()
                }
             }
@@ -111,6 +49,62 @@ blocks.forEach((block)=>{
        };
     });
 });
+
+function shuffle(arr) {
+    let current = arr.length,
+    temp,
+    random;
+    while(current > 0) {
+        random = Math.floor(Math.random() * current);
+        current--;
+        temp = arr[current];
+        arr[current] = arr[random];
+        arr[random] = temp;
+    }
+}
+function flipblock(block) {
+    block.classList.add('is-flipped');
+    let filterblock = blocks.filter(block => block.classList.contains('is-flipped'));
+    console.log(filterblock[0], filterblock[1]);
+    
+    if(filterblock.length === 2) {
+        stopClik();
+        identical(filterblock[0], filterblock[1]);
+    }
+}
+function stopClik() {
+    memoryContainer.classList.add("no-click");
+    setTimeout(()=> {
+        memoryContainer.classList.remove("no-click");
+    },2000)
+}
+function identical(blockOne, blockTwo) {
+    if(blockOne.dataset.technology === blockTwo.dataset.technology) {
+        blockOne.classList.remove('is-flipped');
+        blockTwo.classList.remove('is-flipped');
+        blockOne.classList.add('identical');
+        blockTwo.classList.add('identical');
+        checkAllIdentical ()
+        document.querySelector(".success").play();
+    } else {
+        tries.innerHTML = parseInt(tries.innerHTML) + 1;
+        console.log('no');
+        
+        setTimeout(()=> {
+            blockOne.classList.remove('is-flipped');
+            blockTwo.classList.remove('is-flipped');
+        },2000);
+        document.querySelector(".fail").play()
+    }
+}
+function checkAllIdentical () {
+    let all = blocks.every(block => block.classList.contains('identical'));
+    if(all) {
+        message.className = "popup";
+        message.innerHTML = "You Win!";
+    }
+}
+
 
 
 
